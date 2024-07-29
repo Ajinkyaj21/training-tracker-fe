7762472bcb4aaa714c2aa540dba4d6382e42dcfa
Omkar-Hirve-Flairminds <omkar.hirve@flairminds.com>
2024-07-29 16:00:35 +0530

    Your commit message

--- src/Components/Modals/AddTopic.js
+++ src/Components/Modals/AddTopic.js
@@ -1,25 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './AddTopic.module.css';
 
-export default function addTopic() {
-	return (
-		<div className="modal fade" id="addTopic" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div className="modal-dialog" role="document">
-				<div className="modal-content">
-					<div className="modal-header">
-						<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div className="modal-body">
-                    ...
-					</div>
-					<div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary">Save changes</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+export default function AddTopic({ isOpen, onClose, addNewCourse, data }) {
+  const [formData, setFormData] = useState({
+    moduleName: '',
+    description: '',
+    visitDate: '',
+    topic: '',
+    article: '',
+    youtubeLink: '',
+    practice: '',
+    assignments: '',
+    logo: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      logo: file,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = (data.length + 1).toString();
+
+    const newCourse = {
+      id,
+      name: formData.moduleName,
+      logo: URL.createObjectURL(formData.logo),
+      description: formData.description,
+      lastUpdate: new Date().toLocaleDateString(),
+    };
+    addNewCourse(newCourse);
+    setFormData({
+      moduleName: '',
+      description: '',
+      visitDate: '',
+      topic: '',
+      article: '',
+      youtubeLink: '',
+      practice: '',
+      assignments: '',
+      logo: null,
+    });
+    onClose();
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <div className={styles.modalHeader}>
+              <h5 className={styles.modalTitle}>Add New Course</h5>
+              <button type="button" className={styles.closeButton} onClick={onClose}>&times;</button>
+            </div>
+            <hr className={styles.horizontalLine}/>
+            <div className={styles.modalBody}>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.formGroup}>
+                  <label>Module Name</label>
+                  <input type="text" className={styles.formControl} name="moduleName" value={formData.moduleName} onChange={handleChange} required />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Logo</label>
+                  <input
+                    type="file"
+                    onChange={handleFileChange}
+                    accept="image/*"
+                    className={styles.formControl}
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Description</label>
+                  <textarea className={styles.formControl} name="description" value={formData.description} onChange={handleChange} required />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Visit Date</label>   
+                  <input type="date" className={styles.formControl} name="visitDate" value={formData.visitDate} onChange={handleChange} />                         
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Topic</label>
+                  <input type="text" className={styles.formControl} name="topic" value={formData.topic} onChange={handleChange} required />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Article</label>
+                  <input type="text" className={styles.formControl} name="article" value={formData.article} onChange={handleChange} />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>YouTube Link</label>
+                  <input type="url" className={styles.formControl} name="youtubeLink" value={formData.youtubeLink} onChange={handleChange} />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Practice</label>
+                  <input type="text" className={styles.formControl} name="practice" value={formData.practice} onChange={handleChange} />
+                </div>
+                <div className={styles.formGroup}>
+                  <label>Assignments</label>
+                  <input type="text" className={styles.formControl} name="assignments" value={formData.assignments} onChange={handleChange} />
+                </div>
+                <button type="submit" className={styles.submitButton}>Save changes</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- src/Pages/LearningSpace/LearningSpace.js
+++ src/Pages/LearningSpace/LearningSpace.js
@@ -1,5 +1,4 @@
-import React from 'react';
-// import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import CssLogo from '../../Assets/CssLogo.svg.png';
 import JsIcon from '../../Assets/JsIcon.png';
 import MySQL from '../../Assets/MySQL.svg.png';
@@ -8,75 +7,77 @@ import ReactIcon from '../../Assets/React-icon.svg.png';
 import Button from '../../Components/Button/CustomButton';
 import DisplayBox from '../../Components/DisplayBox/DisplayBox';
 import styles from './LearningSpace.module.css';
+import AddTopic from '../../Components/Modals/AddTopic';
+
 export default function LearningSpace() {
-	// const navigate = useNavigate();
-	// const handelCourse = () => {
-	// 	navigate(`/course`);
-	// };
-	const data = [
-		{id: '1',
-			name: 'MySQL',
-			logo: MySQL,
-			description: "This is a description for the display box.",
-			lastUpdate: "22-07-2011"
-		},
-		{id: '2',
-			name: 'JS',
-			logo: JsIcon,
-			description: "This is a description for the display box.",
-			lastUpdate: "22-07-2011"
-		},
-		{id: '3',
-			name: 'REACT',
-			logo: ReactIcon,
-			description: "This is a description for the display box.",
-			lastUpdate: "22-07-2011"
-		},
-		{id: '4',
-			name: 'CSS',
-			logo: CssLogo,
-			description: "This is a description for the display box.",
-			lastUpdate: "22-07-2011"
-		},
-		{ id: '5',
-			name: 'NODE',
-			logo: NodeLogo,
-			description: "This is a description for the display box.",
-			lastUpdate: "22-07-2011"
-		}
-	];
-	return (
-		<div>
-			<div className={styles.btnDiv}>
-				<Button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">+ Add Course</Button>
-			</div>
-			<div className={styles.mainContainer}>
-				<div className={styles.container1}>
-					{data.map((item, index) => (
-						<DisplayBox key={index} logo={item.logo} name={item.name} description={item.description}
-							lastUpdate={item.lastUpdate} />
-					))}
-				</div>
-			</div>
-			<div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				<div className="modal-dialog" role="document">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-							<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div className="modal-body">
-				...
-						</div>
-						<div className="modal-footer">
-							<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-							<button type="button" className="btn btn-primary">Save changes</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    const initialData = [
+        {
+            id: '1',
+            name: 'MySQL',
+            logo: MySQL,
+            description: "This is a description for the display box.",
+            lastUpdate: "22-07-2011"
+        },
+        {
+            id: '2',
+            name: 'JS',
+            logo: JsIcon,
+            description: "This is a description for the display box.",
+            lastUpdate: "22-07-2011"
+        },
+        {
+            id: '3',
+            name: 'REACT',
+            logo: ReactIcon,
+            description: "This is a description for the display box.",
+            lastUpdate: "22-07-2011"
+        },
+        {
+            id: '4',
+            name: 'CSS',
+            logo: CssLogo,
+            description: "This is a description for the display box.",
+            lastUpdate: "22-07-2011"
+        },
+        {
+            id: '5',
+            name: 'NODE',
+            logo: NodeLogo,
+            description: "This is a description for the display box.",
+            lastUpdate: "22-07-2011"
+        }
+    ];
+
+    const [data, setData] = useState(initialData);
+    const [isAddTopicModalOpen, setIsAddTopicModalOpen] = useState(false);
+
+    const openAddTopic = () => {
+        setIsAddTopicModalOpen(true);
+    };
+
+    const closeAddTopicModal = () => {
+        setIsAddTopicModalOpen(false);
+    };
+
+    const addNewCourse = (newCourse) => {		
+        setData((prevData) => [...prevData, newCourse]);
+		initialData.push(newCourse);
+		console.log("The initial Data is ---> ",initialData);
+    };
+
+    return (
+        <div>
+            <div className={styles.btnDiv}>
+                <Button type="button" className="btn btn-primary" onClick={openAddTopic}>+ Add Course</Button>
+            </div>
+            <div className={styles.mainContainer}>
+                <div className={styles.container1}>
+                    {data.map((item, index) => (
+                        <DisplayBox key={index} logo={item.logo} name={item.name} description={item.description} lastUpdate={item.lastUpdate} />
+                    ))}
+                </div>
+            </div>
+            <AddTopic isOpen={isAddTopicModalOpen} onClose={closeAddTopicModal} addNewCourse={addNewCourse} data={data} />
+        </div>
+    );
 }
